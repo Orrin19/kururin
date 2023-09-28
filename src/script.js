b807148acf20573d66d090af1fbe9bafe4e53c28
Orrin19 <daniil.orrin@yandex.ru>
2023-09-28 16:21:53 +0700

    first commit

---
+++ src/script.js
@@ -0,0 +1,13 @@
+document.querySelector('img').addEventListener('click', () => {
+  document.title = 'YOU GOT KURUKURED!';
+  document.body.classList.toggle('got-kurukured');
+  const audio1 = new Audio('/assets/kuru1.mp3');
+  const audio2 = new Audio('/assets/kuru2.mp3');
+  setInterval(() => {
+    if (Math.floor(Math.random() * 2) == 0) {
+      audio1.play();
+    } else {
+      audio2.play();
+    }
+  }, 500);
+});
