09a22cfff1e48ca5f385f75171fa593cd9bbd17f
Orrin19 <daniil.orrin@yandex.ru>
2023-09-28 16:26:37 +0700

    source fix

--- src/script.js
+++ src/script.js
@@ -1,8 +1,12 @@
 document.querySelector('img').addEventListener('click', () => {
   document.title = 'YOU GOT KURUKURED!';
   document.body.classList.toggle('got-kurukured');
-  const audio1 = new Audio('/assets/kuru1.mp3');
-  const audio2 = new Audio('/assets/kuru2.mp3');
+  const audio1 = new Audio(
+    'https://raw.githubusercontent.com/Orrin19/kururin/main/assets/kuru1.mp3'
+  );
+  const audio2 = new Audio(
+    'https://raw.githubusercontent.com/Orrin19/kururin/main/assets/kuru2.mp3'
+  );
   setInterval(() => {
     if (Math.floor(Math.random() * 2) == 0) {
       audio1.play();
